b70a8533ada9977d09dc11ff6f82ba05595ddb57
Damien Abeloos <dabeloos@w-r2-p2.s19.be>
2018-08-15 15:22:03 +0200

    ex05 ft_strstr

---
+++ ex05/ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/08/15 14:04:40 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/15 15:21:42 by dabeloos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int		i;
+
+	if (to_find[0] == '\0')
+		return (str);
+	while (*str != *to_find && *str != '\0')
+		str++;
+	if (*str == *to_find)
+	{
+		i = 0;
+		while (str[i] != '\0')
+		{
+			if (to_find[i] == '\0')
+				return (str);
+			if (to_find[i] == str[i])
+				i++;
+			else
+				break ;
+		}
+	}
+	return (NULL);
+}
