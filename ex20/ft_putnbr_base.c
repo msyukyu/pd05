aa9dee02c7af7f261dbbe42b1067c963f0169acc
msyukyu <damien_ab@hotmail.com>
2018-08-15 23:02:32 +0200

    ex20 start

--- ex20/ft_putnbr_base.c
+++ ex20/ft_putnbr_base.c
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
 
+int		ft_check_base(char *base)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (base[i] == '+' || base[i] == '-' || base[i] < '?' || base[i] > '~')
+			return (0);
+		j = -1;
+		while (++j < i)
+			if (base[i] == base[j])
+				return (0);
+		i++;
+	}
+	return ((i < 2) ? 0 : i);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+
+}
