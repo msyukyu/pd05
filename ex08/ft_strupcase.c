320e02aa67e5d4fc41823279c1f0bbafa97f988c
Damien Abeloos <dabeloos@w-r7-p5.s19.be>
2018-08-15 17:03:01 +0200

    ex08 ft_strupcase

--- ex08/ft_strupcase.c
+++ ex08/ft_strupcase.c
@@ -6,14 +6,17 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 15:39:12 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/15 15:42:01 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/15 17:02:41 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stdio.h>
-
 char	*ft_strupcase(char *str)
 {
-	while (str++ != '\0')
-		*str = *str + 'A' - 'a';
+	int i;
+
+	i = -1;
+	while (str[++i] != '\0')
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] + 'A' - 'a';
+	return (str);
+}
