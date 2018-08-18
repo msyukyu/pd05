b6679fe5f0d7e0c8939bd15793257c46ec0c6f76
Damien Abeloos <dabeloos@w-r10-p4.s19.be>
2018-08-18 18:15:47 +0200

    strlcat

--- ex18/ft_strlcat.c
+++ ex18/ft_strlcat.c
@@ -6,26 +6,31 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 20:30:00 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/15 21:35:43 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/18 18:15:31 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	l;
+	unsigned int	result;
 	unsigned int	pinned;
 
 	l = 0;
-	while (dest[l] != '\0' && l < size - 1)
+	while (dest[l] != '\0')
 		l++;
 	pinned = l;
+	result = l;
 	l = 0;
-	while (src[l] != '\0' && l + pinned < size)
+	while (src[l] != '\0')
+		l++;
+	result += l;
+	l = 0;
+	while (src[pinned + l] != '\0' && pinned + l < size - 1)
 	{
 		dest[pinned + l] = src[l];
 		l++;
 	}
-	if (l + pinned < size)
-		dest[pinned + l] = '\0';
-	return (l + pinned + 1);
+	dest[pinned + l] = '\0';
+	return (result);
 }
