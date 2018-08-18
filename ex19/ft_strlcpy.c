408d751ad43b1b9cfa05b38e2a80fe5a8754e2aa
Damien Abeloos <dabeloos@w-r10-p4.s19.be>
2018-08-18 18:30:50 +0200

    strlcpy

--- ex19/ft_strlcpy.c
+++ ex19/ft_strlcpy.c
@@ -6,22 +6,25 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 21:37:06 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/15 21:54:29 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/18 18:30:37 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
+	unsigned int	result;
 
 	i = 0;
-	while (dest[i] != '\0' && i < size - 1)
+	while (src[i] != '\0')
+		i++;
+	result = i;
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
 	{
 		dest[i] = src[i];
 		i++;
-		if (src[i - 1] == '\0')
-			return (i);
 	}
 	dest[i] = '\0';
-	return (i + 1);
+	return (result);
 }
