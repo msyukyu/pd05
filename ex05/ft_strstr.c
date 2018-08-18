ca702d6007f22af6deb712d5b9eee4adf3eb1138
Damien Abeloos <dabeloos@w-r6-p7.s19.be>
2018-08-18 14:31:05 +0200

    ex05 finished

--- ex05/ft_strstr.c
+++ ex05/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 14:04:40 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/15 15:21:42 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/18 14:30:52 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,24 @@ char	*ft_strstr(char *str, char *to_find)
 
 	if (to_find[0] == '\0')
 		return (str);
-	while (*str != *to_find && *str != '\0')
-		str++;
-	if (*str == *to_find)
+	while (*str != '\0')
 	{
-		i = 0;
-		while (str[i] != '\0')
+		while (*str != *to_find && *str != '\0')
+			str++;
+		if (*str == *to_find)
 		{
-			if (to_find[i] == '\0')
-				return (str);
-			if (to_find[i] == str[i])
-				i++;
-			else
-				break ;
+			i = 0;
+			while (str[i] != '\0' || (str[i] == '\0' && to_find[i] == '\0'))
+			{
+				if (to_find[i] == '\0')
+					return (str);
+				if (to_find[i] == str[i])
+					i++;
+				else
+					break ;
+			}
 		}
+		str++;
 	}
-	return (NULL);
+	return (str);
 }
