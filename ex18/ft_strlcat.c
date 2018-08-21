66f543208bc38b2f12ae8a3c7351726dd64baff8
Damien Abeloos <dabeloos@w-r9-p5.s19.be>
2018-08-21 12:50:24 +0200

    ft_strlcat

--- ex18/ft_strlcat.c
+++ ex18/ft_strlcat.c
@@ -6,10 +6,12 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 20:30:00 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/20 17:29:08 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/21 12:42:59 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	l;
@@ -21,16 +23,34 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		l++;
 	pinned = l;
 	result = l;
+	if (result == size)
+	{
+		l = 0;
+		while (src[l] != '\0')
+			l++;
+		return (result += l);
+	}
 	l = 0;
 	while (src[l] != '\0')
-		l++;
-	result += l;
-	l = 0;
-	while (src[l] != '\0' && pinned + l < size - 1)
 	{
-		dest[pinned + l] = src[l];
+		if (pinned + l < size - 1)
+			dest[pinned + l] = src[l];
 		l++;
 	}
 	dest[pinned + l] = '\0';
-	return (result);
+	return (result + l);
+}
+
+int		main(void)
+{
+	char dest[] = "truuc\0a123456789";
+	char src[] = "go";
+	char dest2[] = "truuc\0a123456789";
+	char src2[] = "go";
+
+	printf("%d", ft_strlcat(dest, src, 0));
+	printf("%lu", strlcat(dest2, src2, 0));
+	printf("%s", dest);
+	printf("%s", dest2);
+	return (0);
 }
