9c2bb5e72acf978c481b4db8a5d4de10ce51a185
Damien Abeloos <dabeloos@w-r9-p5.s19.be>
2018-08-21 12:52:00 +0200

    ft_strlcat

--- ex18/ft_strlcat.c
+++ ex18/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 20:30:00 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/21 12:42:59 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/21 12:51:49 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
@@ -39,18 +39,4 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	}
 	dest[pinned + l] = '\0';
 	return (result + l);
-}
-
-int		main(void)
-{
-	char dest[] = "truuc\0a123456789";
-	char src[] = "go";
-	char dest2[] = "truuc\0a123456789";
-	char src2[] = "go";
-
-	printf("%d", ft_strlcat(dest, src, 0));
-	printf("%lu", strlcat(dest2, src2, 0));
-	printf("%s", dest);
-	printf("%s", dest2);
-	return (0);
 }
